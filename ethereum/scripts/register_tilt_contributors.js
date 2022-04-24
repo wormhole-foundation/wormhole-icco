6708487721445d79f1b02286fd8c27ffe7002092
Karl Kempe <karlkempe@users.noreply.github.com>
2022-04-24 15:18:05 +0000

    Fix registerChain

--- ethereum/scripts/register_tilt_contributors.js
+++ ethereum/scripts/register_tilt_contributors.js
@@ -5,25 +5,30 @@ const elliptic = require("elliptic");
 const TokenSaleConductor = artifacts.require("TokenSaleConductor");
 const TokenSaleContributor = artifacts.require("TokenSaleContributor");
 
-//sdk.setDefaultWasm("node");
-
 const ConductorImplementationFullABI = jsonfile.readFileSync(
   "../build/contracts/ConductorImplementation.json"
 ).abi;
+
+const fs = require("fs");
+
+const tilt = JSON.parse(
+  fs.readFileSync(`${__dirname}/../../tilt.json`, "utf8")
+);
 
 module.exports = async function(callback) {
   try {
     const emitters = new Map();
     {
+      // TODO: grab this from the tilt.json file...
       // const solanaProgAddr = "22mamxmojFWBdbGqaxTH46HBAgAY2bJRiGJJHfNRNQ95";  //TBD Not used, because I could not get WH sdk to be available in tilt.
       const solanaEmitterAddr =
         "0xaeab35a8d36bbaad38154ca4ca6a0770e7009326316d59ef2c8a2123e90d174c"; // Derived from solanaProgAddr using await sdk.getEmitterAddressSolana(..);
 
       // Build chainId -> ContributorAddr map.
       const ethEmitterAddress =
-        "0x000000000000000000000000" + TokenSaleContributor.address.substr(2);
+        "0x000000000000000000000000" + tilt.ethContributorAddress.substring(2);
       const bscEmitterAddress =
-        "0x000000000000000000000000" + TokenSaleContributor.address.substr(2);
+        "0x000000000000000000000000" + tilt.bscContributorAddress.substring(2);
       emitters.set(1, solanaEmitterAddr);
       emitters.set(2, ethEmitterAddress);
       emitters.set(4, bscEmitterAddress);

f34ec5e991cfd43c2932db0a00d9fabaace062c9
Karl Kempe <karlkempe@users.noreply.github.com>
2022-04-27 22:08:42 +0000

    Add tools

--- Makefile
+++ Makefile
@@ -10,6 +10,11 @@ clean:
 	cd ethereum && make clean
 	cd terra && make clean
 	cd sdk/js && rm -rf node_modules contracts lib src/icco/__tests__/tilt.json
+<<<<<<< Updated upstream
+=======
+	cd tools && rm -rf node_modules lib
+	rm -f tilt.json
+>>>>>>> Stashed changes
 
 .PHONY: ethereum
 ## Build Ethereum contracts
@@ -27,18 +32,37 @@ terra/artifacts/checksum.txt:
 
 .PHONY: sdk
 ## Build SDK
-sdk: ethereum sdk/js/node_modules
+sdk: sdk/js/lib
+
+sdk/js/lib: ethereum sdk/js/node_modules
 	cd sdk/js && npm run build
 
 sdk/js/node_modules:
 	cd sdk/js && npm ci
 
+.PHONY: tools
+## Build tools (scripts in tools directory)
+tools: tools/lib
+
+tools/lib: sdk
+	cd tools && npm ci && npm run build
+
 .PHONY: tilt-deploy
 ## Deploy Contracts to Tilt
+<<<<<<< Updated upstream
 tilt-deploy: ethereum terra
 	@if ! pgrep tilt; then echo "Error: tilt not running. Start it before running tests"; exit 1; fi
 	cd ethereum && make tilt-deploy
 	cd ethereum && npx truffle exec scripts/register_tilt_contributors.js --network eth_devnet
+=======
+tilt-deploy: ethereum tools #terra
+	rm -f tilt.json
+	@if ! pgrep tilt; then echo "Error: tilt not running. Start it before running tests"; exit 1; fi
+	cd ethereum && make tilt-deploy
+#	cd ethereum && npx truffle exec scripts/register_tilt_contributors.js --network eth_devnet
+	node tools/lib/register_tilt_contributors.js
+	cp tilt.json sdk/js/src/icco/__tests__/tilt.json
+>>>>>>> Stashed changes
 
 .PHONY: tilt-test
 ## Run Integration Test in Tilt
---
+++ tools/register_tilt_contributors.ts
@@ -0,0 +1,86 @@
+import fs from "fs";
+
+import {
+  ChainId,
+  CHAIN_ID_SOLANA,
+  CHAIN_ID_ETH,
+  CHAIN_ID_BSC,
+  hexToUint8Array,
+} from "@certusone/wormhole-sdk";
+import {
+  getContributorContractOnEth,
+  registerChainOnEth,
+} from "wormhole-icco-sdk";
+import { ethers } from "ethers";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const ETH_DEVNET_PK =
+  "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"; // account 2
+
+interface Emitter {
+  chainId: ChainId;
+  address: string;
+}
+
+async function main() {
+  const provider = new ethers.providers.StaticJsonRpcProvider(
+    "http://localhost:8545"
+  );
+  const wallet = new ethers.Wallet(ETH_DEVNET_PK, provider);
+
+  const tilt = JSON.parse(
+    fs.readFileSync(`${__dirname}/../../tilt.json`, "utf8")
+  );
+
+  const emitters: Emitter[] = [];
+  {
+    // TODO: grab this from the tilt.json file...
+    // const solanaProgAddr = "22mamxmojFWBdbGqaxTH46HBAgAY2bJRiGJJHfNRNQ95";  //TBD Not used, because I could not get WH sdk to be available in tilt.
+    const solanaEmitterAddr =
+      "aeab35a8d36bbaad38154ca4ca6a0770e7009326316d59ef2c8a2123e90d174c"; // Derived from solanaProgAddr using await sdk.getEmitterAddressSolana(..);
+
+    // Build chainId -> ContributorAddr map.
+    const ethEmitterAddress =
+      "000000000000000000000000" + tilt.ethContributorAddress.substring(2);
+    const bscEmitterAddress =
+      "000000000000000000000000" + tilt.bscContributorAddress.substring(2);
+    emitters.push({ chainId: CHAIN_ID_SOLANA, address: solanaEmitterAddr });
+    emitters.push({ chainId: CHAIN_ID_ETH, address: ethEmitterAddress });
+    emitters.push({ chainId: CHAIN_ID_BSC, address: bscEmitterAddress });
+  }
+
+  // register all chainId -> ContributorAddr with conductor.
+  for (const emitter of emitters) {
+    console.log(
+      "Registering chainId: ",
+      emitter.chainId,
+      " emitter: ",
+      emitter.address
+    );
+
+    const contributorAddress = hexToUint8Array(emitter.address);
+    const receipt = await registerChainOnEth(
+      tilt.conductorAddress,
+      emitter.chainId,
+      contributorAddress,
+      wallet
+    );
+    console.log("tx", receipt.blockHash);
+
+    const check = await getContributorContractOnEth(
+      tilt.conductorAddress,
+      provider,
+      emitter.chainId
+    );
+    console.log(check);
+  }
+
+  return;
+}
+
+main();

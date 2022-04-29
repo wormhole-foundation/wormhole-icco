94f885688f9bfb922879101d22ead1ff714fd621
Karl Kempe <karlkempe@users.noreply.github.com>
2022-04-29 16:05:26 +0000

    Add tests

---
+++ terra/scripts/consts.ts
@@ -0,0 +1,18 @@
+export const WORMHOLE_ADDRESSES: any = {
+  mainnet: {
+    wormhole: "terra1dq03ugtd40zu9hcgdzrsq6z2z4hwhc9tqk2uy5",
+    tokenBridge: "terra10nmmwe8r3g99a9newtqa7a75xfgs2e8z87r2sf",
+  },
+  testnet: {
+    wormhole: "terra1pd65m0q9tl3v8znnz5f5ltsfegyzah7g42cx5v",
+    tokenBridge: "terra1pseddrv0yfsn76u4zxrjmtf45kdlmalswdv39a",
+  },
+  localterra: {
+    wormhole: "terra18vd8fpwxzck93qlwghaj6arh4p7c5n896xzem5",
+    tokenBridge: "terra10pyejy66429refv3g35g2t7am0was7ya7kz2a4",
+  },
+  tilt: {
+    wormhole: "terra18vd8fpwxzck93qlwghaj6arh4p7c5n896xzem5",
+    tokenBridge: "terra10pyejy66429refv3g35g2t7am0was7ya7kz2a4",
+  },
+};
---
+++ terra/scripts/tests/tests.ts
@@ -0,0 +1,142 @@
+import { LCDClient, Wallet } from "@terra-money/terra.js";
+import { WORMHOLE_ADDRESSES } from "../consts";
+import {
+  instantiateContract,
+  newLocalClient,
+  uploadContract,
+} from "../helpers";
+
+const contracts = new Map<string, string>();
+
+async function main() {
+  const { terra, wallet } = newLocalClient();
+
+  console.log("-------- Deployment --------\n");
+  await deployment(terra, wallet);
+  console.log();
+
+  console.log("-------- Conduct Successful Sale --------\n");
+  await conductSuccessfulSale(terra, wallet);
+  console.log();
+
+  console.log("-------- Conduct Aborted Sale --------\n");
+  await conductAbortedSale(terra, wallet);
+  console.log();
+  return;
+}
+
+function logTestName(test: string): void {
+  console.log("\x1b[33m%s\x1b[0m", test);
+}
+
+function success(): void {
+  console.log("... \x1b[32msuccess!\x1b[0m");
+}
+
+function untested(): void {
+  console.log("... \x1b[31muntested\x1b[0m");
+}
+
+async function deployment(terra: LCDClient, wallet: Wallet): Promise<void> {
+  {
+    logTestName("1. Deploy Contract");
+    const addresses = WORMHOLE_ADDRESSES.localterra;
+
+    const codeId = await uploadContract(
+      terra,
+      wallet,
+      "../artifacts/icco_contributor.wasm"
+    );
+
+    const contributor = await instantiateContract(
+      terra,
+      wallet,
+      wallet.key.accAddress,
+      codeId,
+      {
+        wormhole: addresses.wormhole,
+        token_bridge: addresses.tokenBridge,
+        conductor_chain: 2,
+        conductor_address: "",
+      }
+    );
+    contracts.set("contributor", contributor);
+    success();
+  }
+
+  {
+    logTestName("2. Upgrade Contract");
+    untested();
+  }
+
+  // done
+  return;
+}
+
+async function conductSuccessfulSale(
+  terra: LCDClient,
+  wallet: Wallet
+): Promise<void> {
+  const sale = {};
+
+  {
+    logTestName("1. Orchestrator Initializes Sale... ");
+    untested();
+  }
+
+  {
+    logTestName("2. User Contributes to Sale");
+    untested();
+  }
+
+  {
+    logTestName("3. Orchestrator Attests Contributions");
+    untested();
+  }
+
+  {
+    logTestName("4. Orchestrator Seals Sale");
+    untested();
+  }
+
+  {
+    logTestName("5. User Claims Allocations");
+    untested();
+  }
+
+  return;
+}
+
+async function conductAbortedSale(
+  terra: LCDClient,
+  wallet: Wallet
+): Promise<void> {
+  {
+    logTestName("1. Orchestrator Initializes Sale");
+    untested();
+  }
+
+  {
+    logTestName("2. User Contributes to Sale");
+    untested();
+  }
+
+  {
+    logTestName("3. Orchestrator Attests Contributions");
+    untested();
+  }
+
+  {
+    logTestName("4. Orchestrator Aborts Sale");
+    untested();
+  }
+
+  {
+    logTestName("5. User Claims Refunds");
+    untested();
+  }
+
+  return;
+}
+
+main();

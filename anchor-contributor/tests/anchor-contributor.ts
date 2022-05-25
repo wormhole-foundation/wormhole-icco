dda1b248afa31b35a29464034f117fd61b4b39b6
Karl Kempe <karlkempe@users.noreply.github.com>
2022-05-25 14:17:09 +0000

    Add sign and encode vaa

--- anchor-contributor/tests/anchor-contributor.ts
+++ anchor-contributor/tests/anchor-contributor.ts
@@ -2,80 +2,143 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { AnchorContributor } from "../target/types/anchor_contributor";
 import {
-  tryNativeToUint8Array, 
+  tryNativeToUint8Array,
   postVaaSolanaWithRetry,
   importCoreWasm,
-  setDefaultWasm
-} from '@certusone/wormhole-sdk';
+  setDefaultWasm,
+} from "@certusone/wormhole-sdk";
 import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
-import fs from 'fs';
-import * as b from 'byteify';
+import fs from "fs";
+import * as b from "byteify";
 import keccak256 from "keccak256";
-import {parseSaleInit} from "../../sdk/js/src/icco/signedVaa";
+import { parseSaleInit } from "../../sdk/js/src/icco/signedVaa";
+import { encodeSaleInit, signAndEncodeVaa } from "./lib";
 
 describe("anchor-contributor", () => {
-    // Configure the client to use the local cluster.
-    anchor.setProvider(anchor.AnchorProvider.env());
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
 
-    const program = anchor.workspace.AnchorContributor as Program<AnchorContributor>;
+  const program = anchor.workspace.AnchorContributor as Program<AnchorContributor>;
 
-    const CONDUCTOR_CHAIN = 2;
-    const CONDUCTOR_ADDRESS = tryNativeToUint8Array('0x5c49f34D92316A2ac68d10A1e2168e16610e84f9', "ethereum");
-    const owner = anchor.web3.Keypair.fromSecretKey(Uint8Array.from(JSON.parse(fs.readFileSync("./tests/test_keypair.json").toString())))
-    const CORE_BRIDGE_ADDRESS = new anchor.web3.PublicKey("Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o");
+  const CONDUCTOR_CHAIN = 2;
+  const CONDUCTOR_ADDRESS = tryNativeToUint8Array(
+    "0x5c49f34D92316A2ac68d10A1e2168e16610e84f9",
+    "ethereum"
+  );
+  const owner = anchor.web3.Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(fs.readFileSync("./tests/test_keypair.json").toString()))
+  );
+  const CORE_BRIDGE_ADDRESS = new anchor.web3.PublicKey(
+    "Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o"
+  );
 
-    const [contributor_acc, contributor_bmp] = findProgramAddressSync([
-        Buffer.from("contributor"),
-        CONDUCTOR_ADDRESS
-    ], program.programId)
+  const [contributor_acc, contributor_bmp] = findProgramAddressSync(
+    [Buffer.from("contributor"), CONDUCTOR_ADDRESS],
+    program.programId
+  );
 
-    it("creates conductor", async () => {
-        await program.methods
-        .createContributor(
-            CONDUCTOR_CHAIN,
-            Buffer.from(CONDUCTOR_ADDRESS)
-        )
-        .accounts({
-            owner: owner.publicKey,
-            contributor: contributor_acc,
-            systemProgram: anchor.web3.SystemProgram.programId
-        })
-        .rpc()
+  it("creates conductor", async () => {
+    await program.methods
+      .createContributor(CONDUCTOR_CHAIN, Buffer.from(CONDUCTOR_ADDRESS))
+      .accounts({
+        owner: owner.publicKey,
+        contributor: contributor_acc,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
 
-        console.log(await program.account.contributor.fetch(contributor_acc));
+    console.log(await program.account.contributor.fetch(contributor_acc));
   });
 
   it("initalizes a sale", async () => {
     //console.log(await program.provider.connection.getAccountInfo(new anchor.web3.PublicKey("3u8hJUVTA4jH1wYAyUur7FFZVQ8H635K3tSHHF4ssjQ5")));
 
-    const initSaleVaa = "01000000000100ef188aec456b284bc9f94a8d229a037eb83ffbdf29191ef12a56097f3d39c1bc2c4c27bcf1b84c38637d0f043bea8c462faf12cb34f2d70555b5930d6e2f8b6d01000006d70000000000020000000000000000000000004cfb3f70bf6a80397c2e634e5bdd85bc0bb189ee00000000000000010f010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000ac92a45c2b0ce520e12dd696af589073e86b2f470002120000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000c249fdd32778000000000000000000000000000000000000000000000000000000000000000006d8000000000000000000000000000000000000000000000000000000000000071405000000000000000000000000ddb64fe46a91d46ee29420539fc25fd07c5fea3e000200000000000000000de0b6b3a7640000000000000000000000000000ddb64fe46a91d46ee29420539fc25fd07c5fea3e0004000000000000000002c68af0bb1400000000000000000000000000008a5bbc20ad253e296f61601e868a3206b2d4774c0002000000000000000002c68af0bb1400000000000000000000000000003d9e7a12daa29a8b2b1bfaa9dc97ce018853ab31000400000000000000000de0b6b3a7640000165809739240a0ac03b98440fe8985548e3aa683cd0d4d9df5b5659669faa301000100000000000000000de0b6b3a7640000000000000000000000000000ac92a45c2b0ce520e12dd696af589073e86b2f4700000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b00000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b"
+    // set up saleInit vaa
+    const saleId = 1;
+    const tokenAddress = "00000000000000000000000083752ecafebf4707258dedffbd9c7443148169db";
+    const tokenChain = 2;
+    const tokenAmount = "1000000000000000000";
+    const minRaise = "10000000000000000000";
+    const maxRaise = "14000000000000000000";
+
+    // set up sale time based on block time
+    const blockTime = await getBlockTime(terra);
+    const saleStart = blockTime + 5;
+    const saleEnd = blockTime + 35;
+
+    const acceptedTokens: AcceptedToken[] = [
+      {
+        address: nativeToHexString("uluna", CHAIN_ID_TERRA)!,
+        chain: 3,
+        conversionRate: "1000000000000000000",
+      },
+      {
+        address: nativeToHexString(mockToken, CHAIN_ID_TERRA)!,
+        chain: 3,
+        conversionRate: "200000000000000000",
+      },
+    ];
+
+    const recipient = "00000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b";
+    const refundRecipient = "00000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b";
+
+    const data = encodeSaleInit(
+      saleId,
+      tokenAddress,
+      tokenChain,
+      tokenAmount,
+      minRaise,
+      maxRaise,
+      saleStart,
+      saleEnd,
+      acceptedTokens,
+      recipient,
+      refundRecipient
+    );
+
+    const timestamp = 1;
+    const nonce = 0;
+    const sequence = 3;
+
+    const initSaleVaa = signAndEncodeVaa(
+      //return signAndEncodeVaaLegacy(
+      timestamp,
+      nonce,
+      CONDUCTOR_CHAIN,
+      Buffer.from(CONDUCTOR_ADDRESS).toString("hex"),
+      sequence,
+      data
+    ).toString("hex");
+
+    //const initSaleVaa =
+    //  "01000000000100ef188aec456b284bc9f94a8d229a037eb83ffbdf29191ef12a56097f3d39c1bc2c4c27bcf1b84c38637d0f043bea8c462faf12cb34f2d70555b5930d6e2f8b6d01000006d70000000000020000000000000000000000004cfb3f70bf6a80397c2e634e5bdd85bc0bb189ee00000000000000010f010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000ac92a45c2b0ce520e12dd696af589073e86b2f470002120000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000008ac7230489e80000000000000000000000000000000000000000000000000000c249fdd32778000000000000000000000000000000000000000000000000000000000000000006d8000000000000000000000000000000000000000000000000000000000000071405000000000000000000000000ddb64fe46a91d46ee29420539fc25fd07c5fea3e000200000000000000000de0b6b3a7640000000000000000000000000000ddb64fe46a91d46ee29420539fc25fd07c5fea3e0004000000000000000002c68af0bb1400000000000000000000000000008a5bbc20ad253e296f61601e868a3206b2d4774c0002000000000000000002c68af0bb1400000000000000000000000000003d9e7a12daa29a8b2b1bfaa9dc97ce018853ab31000400000000000000000de0b6b3a7640000165809739240a0ac03b98440fe8985548e3aa683cd0d4d9df5b5659669faa301000100000000000000000de0b6b3a7640000000000000000000000000000ac92a45c2b0ce520e12dd696af589073e86b2f4700000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b00000000000000000000000022d491bde2303f2f43325b2108d26f1eaba1e32b";
     setDefaultWasm("node");
-    const {parse_vaa} = await importCoreWasm();
+    const { parse_vaa } = await importCoreWasm();
     const parsedVaa = parse_vaa(Buffer.from(initSaleVaa, "hex"));
     //console.log(JSON.stringify(parse_vaa(Buffer.from(initSaleVaa, "hex")), null, 2));
 
     //Post VAA to Core Bridge
     await postVaaSolanaWithRetry(
-        program.provider.connection,
-        async (tx) => {
-            tx.partialSign(owner);
-            return tx;
-        },
-        CORE_BRIDGE_ADDRESS.toString(),
-        owner.publicKey.toString(),
-        Buffer.from(initSaleVaa, "hex"),
-        10
+      program.provider.connection,
+      async (tx) => {
+        tx.partialSign(owner);
+        return tx;
+      },
+      CORE_BRIDGE_ADDRESS.toString(),
+      owner.publicKey.toString(),
+      Buffer.from(initSaleVaa, "hex"),
+      10
     );
     await new Promise((r) => setTimeout(r, 5000));
 
-    const [sale_acc, sale_bmp] = findProgramAddressSync([
-        Buffer.from("icco-sale"),
-        parsedVaa.payload.slice(1,33)
-    ], program.programId);
+    const [sale_acc, sale_bmp] = findProgramAddressSync(
+      [Buffer.from("icco-sale"), parsedVaa.payload.slice(1, 33)],
+      program.programId
+    );
 
     //Find the Core Bridge VAA address (uses hash of)
     //Create VAA Hash to use in core bridge key
-    let buffer_array = []
+    let buffer_array = [];
     buffer_array.push(b.serializeUint32(parsedVaa.timestamp));
     buffer_array.push(b.serializeUint32(parsedVaa.nonce));
     buffer_array.push(b.serializeUint16(parsedVaa.emitter_chain));
@@ -85,25 +148,44 @@ describe("anchor-contributor", () => {
     buffer_array.push(Uint8Array.from(parsedVaa.payload));
     const hash = keccak256(Buffer.concat(buffer_array));
 
-    let core_bridge_vaa_key = findProgramAddressSync([
-        Buffer.from("PostedVAA"),
-        hash
-    ], CORE_BRIDGE_ADDRESS)[0]
-    console.log("Core Bridge VAA: ", await program.provider.connection.getAccountInfo(core_bridge_vaa_key));
+    let core_bridge_vaa_key = findProgramAddressSync(
+      [Buffer.from("PostedVAA"), hash],
+      CORE_BRIDGE_ADDRESS
+    )[0];
+    console.log(
+      "Core Bridge VAA: ",
+      await program.provider.connection.getAccountInfo(core_bridge_vaa_key)
+    );
 
-    
     // Call Init Sale
     await program.methods
-        .initSale()
-        .accounts({
-            contributor: contributor_acc,
-            sale: sale_acc,
-            coreBridgeVaa: core_bridge_vaa_key,
-            owner: owner.publicKey,
-            systemProgram: anchor.web3.SystemProgram.programId
-        })
-        .rpc()
-    
+      .initSale()
+      .accounts({
+        contributor: contributor_acc,
+        sale: sale_acc,
+        coreBridgeVaa: core_bridge_vaa_key,
+        owner: owner.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
     console.log(await program.account.sale.fetch(sale_acc));
   });
 });
+
+export function signAndEncodeConductorVaa(
+  timestamp: number,
+  nonce: number,
+  sequence: number,
+  data: Buffer
+): Buffer {
+  return signAndEncodeVaa(
+    //return signAndEncodeVaaLegacy(
+    timestamp,
+    nonce,
+    CONDUCTOR_CHAIN,
+    CONDUCTOR_ADDRESS,
+    sequence,
+    data
+  );
+}

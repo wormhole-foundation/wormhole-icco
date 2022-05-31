c5ba8a337f02a27935d6edb1fc28a69888858704
Karl Kempe <karlkempe@users.noreply.github.com>
2022-05-31 15:54:18 +0000

    Fix consts

--- anchor-contributor/tests/helpers/consts.ts
+++ anchor-contributor/tests/helpers/consts.ts
@@ -1,9 +1,9 @@
-import { CHAIN_ID_ETH, tryNativeToUint8Array } from "@certusone/wormhole-sdk";
+import { CHAIN_ID_ETH, hexToUint8Array } from "@certusone/wormhole-sdk";
 import { web3 } from "@project-serum/anchor";
 
 // wormhole
 export const CORE_BRIDGE_ADDRESS = new web3.PublicKey("Bridge1p5gheXUvJ6jGWGeCsgPKgnE3YgdGKRVCMY9o");
 
 // contributor
-export const CONDUCTOR_CHAIN = CHAIN_ID_ETH as number;
-export const CONDUCTOR_ADDRESS = tryNativeToUint8Array("0x5c49f34D92316A2ac68d10A1e2168e16610e84f9", CHAIN_ID_ETH);
+export const CONDUCTOR_CHAIN: number = parseInt(process.env.CONDUCTOR_CHAIN);
+export const CONDUCTOR_ADDRESS: Uint8Array =  hexToUint8Array(process.env.CONDUCTOR_ADDRESS);

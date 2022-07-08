44e5b41dfd7b48fa81345c37abd71bb9cf711662
Yasir Iqbal <yasiriqbal776@gmail.com>
2022-07-08 17:19:15 +0500

    EVM Events Update

--- ethereum/contracts/icco/contributor/ContributorState.sol
+++ ethereum/contracts/icco/contributor/ContributorState.sol
@@ -26,17 +26,20 @@ contract ContributorEvents {
 
     event EventClaimAllocation (
         uint256 saleId,
-        uint256 tokenIndex
+        uint256 tokenIndex,
+        uint256 amount
     );
 
     event EventClaimRefund (
         uint256 saleId,
-        uint256 tokenIndex
+        uint256 tokenIndex,
+        uint256 amount
     );
 
     event EventClaimExcessContribution (
         uint256 saleId,
-        uint256 tokenIndex
+        uint256 tokenIndex,
+        uint256 amount
     );
 }
 
